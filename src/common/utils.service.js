e09c5d5b1b92d82af14e0a598f0e3c0a799d32ca
Aliaksei Habruseu <aliaksei.habruseu@aerlingus.com>
2016-07-07 15:39:58 +0300

    Meals list

--- src/common/utils.service.js
+++ src/common/utils.service.js
@@ -1,9 +1,18 @@
 class UtilsService {
-    constructor() {
-        
+    constructor() {        
+        this.config = {
+            img_rsr: "https://images.aerlingus.com/resrc-origin/",
+            host: "https://www.aerlingus.com"
+        };
     }
     parseResponse(resp) {
         return resp.data.data;
+    }    
+    getConfig() {
+        return this.config;
+    }
+    getRsrImage(src, width, height) {
+        return this.config.img_rsr + 'c=ar1410x' + height + '/s=w' + width + ',pd1/o=75/' + src;
     }
 }
 
--- src/meals/meals.controller.js
+++ src/meals/meals.controller.js
@@ -6,12 +6,14 @@ class MealsController {
         this.activeMeals = [];
         this.UtilsService = UtilsService;
         this.ProductsService = ProductsService;        
+        this.config = this.UtilsService.getConfig();
+        this.dataObj = {meals: []};
         this.initMeals();
     }
     initMeals() {        
         this.ProductsService.getMeals().then(meals => {
             let p_data = this.UtilsService.parseResponse(meals)[0].meals;
-            this.scope.meals = p_data.mealsOnSegments[0].passengerMealList;
+            this.scope.meals = p_data.mealsOnSegments[0].passengerMealList;            
 
             this.scope.$watch('activePassangerId', (val) => {
                 this.activeMeals = this.scope.meals[val - 1].mealList;
@@ -19,7 +21,16 @@ class MealsController {
             });
 
         });
-    }    
+    }
+    changePassanger(passanger) {
+        this.scope.activePassangerId = passanger.passengerId;
+    }
+    getImg(src, width, height) {
+        return this.UtilsService.getRsrImage(src, width, height);
+    }
+    addMeal(meal) {
+        
+    }
 }
 
 export {MealsController};
